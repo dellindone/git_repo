2de35e3941d863d325f6a48f42f9a092f1389ed6
dellindone <adityav961@gmail.com>
2024-11-30 20:02:59 +0530

    complete django project end to end.

--- chaiaurDjango/chai/views.py
+++ chaiaurDjango/chai/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import ChaiVerity
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
 def all_chai(request):
-    return render(request, 'chai/all_chai.html')
+    chais = ChaiVerity.objects.all()
+    return render(request, 'chai/all_chai.html', {'chais': chais})
+
+def chai_detail(request, chai_id):
+    chai = get_object_or_404(ChaiVerity, pk=chai_id)
+    return render(request, 'chai/chai_details.html', {'chai': chai})
